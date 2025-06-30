0de40d9d4411e6d32b483e9f66f87abbbe141ca8
xI-Mx-Ix <downhill.xmx@gmail.com>
2025-06-30 21:45:18 +0200

    Fixed broken vertices

--- src/main/java/net/xmx/xbullet/builtin/cloth/ClothSoftBodyRenderer.java
+++ src/main/java/net/xmx/xbullet/builtin/cloth/ClothSoftBodyRenderer.java
@@ -33,11 +33,13 @@ public class ClothSoftBodyRenderer extends SoftPhysicsObject.Renderer {
         int heightSegments = data.getSyncedNbtData().getInt("heightSegments");
         if (widthSegments <= 0 || heightSegments <= 0) return;
 
+        Vec3 cameraPos = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
+
         int numVerticesX = widthSegments + 1;
         VertexConsumer buffer = bufferSource.getBuffer(RenderType.solid());
         TextureAtlasSprite sprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(BLUE_WOOL_TEXTURE);
 
-        BiFunction<Integer, Integer, Vector3f> getVertexLocalPos = (x, y) -> {
+        BiFunction<Integer, Integer, Vector3f> getVertexWorldPos = (x, y) -> {
             int index = (y * numVerticesX + x) * 3;
             if (index + 2 >= renderVertexData.length) return new Vector3f();
             return new Vector3f(renderVertexData[index], renderVertexData[index + 1], renderVertexData[index + 2]);
@@ -45,38 +47,31 @@ public class ClothSoftBodyRenderer extends SoftPhysicsObject.Renderer {
 
         for (int y = 0; y < heightSegments; ++y) {
             for (int x = 0; x < widthSegments; ++x) {
+                Vector3f v1_world = getVertexWorldPos.apply(x, y);
+                Vector3f v2_world = getVertexWorldPos.apply(x + 1, y);
+                Vector3f v3_world = getVertexWorldPos.apply(x + 1, y + 1);
+                Vector3f v4_world = getVertexWorldPos.apply(x, y + 1);
 
-                Vector3f v1 = getVertexLocalPos.apply(x, y);
-                Vector3f v2 = getVertexLocalPos.apply(x + 1, y);
-                Vector3f v3 = getVertexLocalPos.apply(x + 1, y + 1);
-                Vector3f v4 = getVertexLocalPos.apply(x, y + 1);
+                Vector3f v1 = new Vector3f(v1_world).sub((float)cameraPos.x, (float)cameraPos.y, (float)cameraPos.z);
+                Vector3f v2 = new Vector3f(v2_world).sub((float)cameraPos.x, (float)cameraPos.y, (float)cameraPos.z);
+                Vector3f v3 = new Vector3f(v3_world).sub((float)cameraPos.x, (float)cameraPos.y, (float)cameraPos.z);
+                Vector3f v4 = new Vector3f(v4_world).sub((float)cameraPos.x, (float)cameraPos.y, (float)cameraPos.z);
 
                 float u1 = sprite.getU((float) x / widthSegments * 16f);
                 float u2 = sprite.getU((float) (x + 1) / widthSegments * 16f);
                 float v1Coord = sprite.getV((float) y / heightSegments * 16f);
                 float v2Coord = sprite.getV((float) (y + 1) / heightSegments * 16f);
+                Vector3f edge1 = new Vector3f(v2).sub(v1);
+                Vector3f edge2 = new Vector3f(v4).sub(v1);
+                Vector3f normal = edge1.cross(edge2).normalize();
+                if (Float.isNaN(normal.x())) normal.set(0, 1, 0);
 
-                Vector3f normal1 = calculateNormal(v1, v2, v3);
-                addVertex(buffer, poseStack, v1, u1, v1Coord, normal1, packedLight);
-                addVertex(buffer, poseStack, v2, u2, v1Coord, normal1, packedLight);
-                addVertex(buffer, poseStack, v3, u2, v2Coord, normal1, packedLight);
-
-                Vector3f normal2 = calculateNormal(v1, v3, v4);
-                addVertex(buffer, poseStack, v1, u1, v1Coord, normal2, packedLight);
-                addVertex(buffer, poseStack, v3, u2, v2Coord, normal2, packedLight);
-                addVertex(buffer, poseStack, v4, u1, v2Coord, normal2, packedLight);
+                addVertex(buffer, poseStack, v1, u1, v1Coord, normal, packedLight);
+                addVertex(buffer, poseStack, v2, u2, v1Coord, normal, packedLight);
+                addVertex(buffer, poseStack, v3, u2, v2Coord, normal, packedLight);
+                addVertex(buffer, poseStack, v4, u1, v2Coord, normal, packedLight);
             }
         }
-    }
-
-    private Vector3f calculateNormal(Vector3f v1, Vector3f v2, Vector3f v3) {
-        Vector3f edge1 = new Vector3f(v2).sub(v1);
-        Vector3f edge2 = new Vector3f(v3).sub(v1);
-        Vector3f normal = edge1.cross(edge2).normalize();
-        if (Float.isNaN(normal.x()) || normal.length() == 0) {
-            normal.set(0, 1, 0);
-        }
-        return normal;
     }
 
     private void addVertex(VertexConsumer buffer, PoseStack poseStack, Vector3f pos, float u, float v, Vector3f normal, int packedLight) {
