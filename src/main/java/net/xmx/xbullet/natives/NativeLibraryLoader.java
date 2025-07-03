4bfbad3d833d193cb543bf4cbcb454a9c636beb6
xI-Mx-Ix <downhill.xmx@gmail.com>
2025-07-03 10:55:17 +0200

    Prepare build setup for Shadow plugin

--- src/main/java/net/xmx/xbullet/natives/NativeLibraryLoader.java
+++ src/main/java/net/xmx/xbullet/natives/NativeLibraryLoader.java
@@ -42,9 +42,9 @@ public class NativeLibraryLoader {
 
                 NativeBinaryLoader loader = new NativeBinaryLoader(info);
 
+
                 NativeDynamicLibrary[] libraries = {
                         new NativeDynamicLibrary("linux/aarch64/com/github/stephengold", PlatformPredicate.LINUX_ARM_64),
-                        new NativeDynamicLibrary("linux/armhf/com/github/stephengold", PlatformPredicate.LINUX_ARM_32),
                         new NativeDynamicLibrary("linux/x86-64/com/github/stephengold", PlatformPredicate.LINUX_X86_64),
                         new NativeDynamicLibrary("osx/aarch64/com/github/stephengold", PlatformPredicate.MACOS_ARM_64),
                         new NativeDynamicLibrary("osx/x86-64/com/github/stephengold", PlatformPredicate.MACOS_X86_64),
