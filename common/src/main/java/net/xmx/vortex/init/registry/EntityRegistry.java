95b89625e2afd7607c9ecb1df4ae841e67ebeba7
xI-Mx-Ix <downhill.xmx@gmail.com>
2025-07-29 02:08:56 +0200

    Remove unnecessary tests

--- common/src/main/java/net/xmx/vortex/init/registry/EntityRegistry.java
+++ common/src/main/java/net/xmx/vortex/init/registry/EntityRegistry.java
@@ -6,18 +6,8 @@ import net.minecraft.client.renderer.entity.NoopRenderer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.xmx.vortex.physics.object.riding.RidingProxyEntity;
-import net.xmx.vortex.test.ExampleCarEntity;
-import net.xmx.vortex.test.ExampleCarRenderer;
 
 public class EntityRegistry {
-
-    public static final RegistrySupplier<EntityType<ExampleCarEntity>> EXAMPLE_CAR =
-            ModRegistries.ENTITY_TYPES.register(
-                    "example_car",
-                    () -> EntityType.Builder.of(ExampleCarEntity::new, MobCategory.MISC)
-                            .sized(1.0F, 1.0F)
-                            .build("example_car")
-            );
 
     public static final RegistrySupplier<EntityType<RidingProxyEntity>> RIDING_PROXY =
             ModRegistries.ENTITY_TYPES.register(
@@ -32,7 +22,6 @@ public class EntityRegistry {
     public static void register() {
         ModRegistries.ENTITY_TYPES.register();
 
-        EntityRendererRegistry.register(EntityRegistry.EXAMPLE_CAR, ExampleCarRenderer::new);
         EntityRendererRegistry.register(EntityRegistry.RIDING_PROXY, NoopRenderer::new);
     }
 }
