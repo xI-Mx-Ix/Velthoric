7e6b3447b079999c4c26970a2e92a1a2463df01a
xI-Mx-Ix <downhill.xmx@gmail.com>
2025-08-10 16:23:56 +0200

    Rename item to vortex:box_thrower

--- common/src/main/java/net/xmx/vortex/init/registry/ItemRegistry.java
+++ common/src/main/java/net/xmx/vortex/init/registry/ItemRegistry.java
@@ -14,7 +14,7 @@ public class ItemRegistry {
     public static final RegistrySupplier<Item> PHYSICS_REMOVER_STICK = ModRegistries.ITEMS.register("physics_remover", PhysicsRemoverItem::new);
     public static final RegistrySupplier<Item> PHYSICS_GUN = ModRegistries.ITEMS.register("physics_gun", PhysicsGunItem::new);
     public static final RegistrySupplier<Item> MAGNETIZER = ModRegistries.ITEMS.register("magnetizer", MagnetizerItem::new);
-    public static final RegistrySupplier<Item> BOX_THROWER = ModRegistries.ITEMS.register("boxthrower", BoxThrowertem::new);
+    public static final RegistrySupplier<Item> BOX_THROWER = ModRegistries.ITEMS.register("box_thrower", BoxThrowertem::new);
 
     public static void register() {
         ModRegistries.ITEMS.register();
