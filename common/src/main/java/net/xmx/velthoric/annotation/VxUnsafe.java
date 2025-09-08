d5d784ea6369ab48ad9e849436717db34b977372
xI-Mx-Ix <downhill.xmx@gmail.com>
2025-09-08 18:59:43 +0200

    feat: add VxUnsafe annotation

---
+++ common/src/main/java/net/xmx/velthoric/annotation/VxUnsafe.java
@@ -0,0 +1,15 @@
+package net.xmx.velthoric.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a method as unsafe. Use with caution: it can break things if used incorrectly.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+public @interface VxUnsafe {
+    String value() default "This method is unsafe. Use at your own risk.";
+}
