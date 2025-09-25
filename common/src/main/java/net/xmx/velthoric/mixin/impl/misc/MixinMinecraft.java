9b7fd88b569253fdeb32fb5dbccb978adc4b4123
xI-Mx-Ix <xmxv.wz@gmail.com>
2025-09-25 22:54:05 +0200

    fix: Prevent physics pause on integrated LAN servers

--- common/src/main/java/net/xmx/velthoric/mixin/impl/misc/MixinMinecraft.java
+++ common/src/main/java/net/xmx/velthoric/mixin/impl/misc/MixinMinecraft.java
@@ -5,11 +5,13 @@
 package net.xmx.velthoric.mixin.impl.misc;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.server.IntegratedServer;
 import net.xmx.velthoric.init.VxMainClass;
-
 import net.xmx.velthoric.physics.object.client.time.VxClientClock;
 import net.xmx.velthoric.physics.world.VxPhysicsWorld;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,20 +23,25 @@ public abstract class MixinMinecraft {
     @Unique
     private static boolean velthoric$wasPaused = false;
 
+    @Shadow
+    @Nullable
+    private IntegratedServer singleplayerServer;
+
     @Inject(method = "runTick", at = @At("HEAD"))
     private void velthoric$onRunTick(boolean renderLevel, CallbackInfo ci) {
         Minecraft mc = (Minecraft) (Object) this;
-
         VxClientClock clientClock = VxClientClock.getInstance();
 
-        if (!mc.isSingleplayer()) {
-            if (velthoric$wasPaused) {
-                VxMainClass.LOGGER.debug("Left singleplayer world, resetting physics pause state.");
-                velthoric$wasPaused = false;
+        boolean isGamePausable = this.singleplayerServer != null && !this.singleplayerServer.isPublished();
 
+        if (!isGamePausable) {
+            if (velthoric$wasPaused) {
+                VxMainClass.LOGGER.debug("Game is no longer pausable (e.g., opened to LAN). Ensuring physics is running.");
+                VxPhysicsWorld.getAll().forEach(VxPhysicsWorld::resume);
                 clientClock.resume();
-                clientClock.reset();
+                velthoric$wasPaused = false;
             }
+
             return;
         }
 
@@ -42,14 +49,12 @@ public abstract class MixinMinecraft {
 
         if (isNowPaused != velthoric$wasPaused) {
             if (isNowPaused) {
-                VxMainClass.LOGGER.debug("Integrated server game is pausing physics...");
+                VxMainClass.LOGGER.debug("Single-player game paused. Pausing physics simulation...");
                 VxPhysicsWorld.getAll().forEach(VxPhysicsWorld::pause);
-
                 clientClock.pause();
             } else {
-                VxMainClass.LOGGER.debug("Integrated server game is resuming physics...");
+                VxMainClass.LOGGER.debug("Single-player game resumed. Resuming physics simulation...");
                 VxPhysicsWorld.getAll().forEach(VxPhysicsWorld::resume);
-
                 clientClock.resume();
             }
             velthoric$wasPaused = isNowPaused;
