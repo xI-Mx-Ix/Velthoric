cb34e5279e0d85a6ba4801149d5c7ef22e0c046d
xI-Mx-Ix <xmxv.wz@gmail.com>
2025-09-29 02:03:12 +0200

    feat: Add data serializers for common Jolt types

--- common/src/main/java/net/xmx/velthoric/physics/object/sync/VxDataSerializers.java
+++ common/src/main/java/net/xmx/velthoric/physics/object/sync/VxDataSerializers.java
@@ -4,8 +4,16 @@
  */
 package net.xmx.velthoric.physics.object.sync;
 
+import com.github.stephengold.joltjni.Color;
+import com.github.stephengold.joltjni.Float2;
+import com.github.stephengold.joltjni.Float3;
+import com.github.stephengold.joltjni.Plane;
 import com.github.stephengold.joltjni.Quat;
 import com.github.stephengold.joltjni.RVec3;
+import com.github.stephengold.joltjni.UVec4;
+import com.github.stephengold.joltjni.Vec3;
+import com.github.stephengold.joltjni.Vec4;
+import com.github.stephengold.joltjni.VertexList;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.xmx.velthoric.network.VxByteBuf;
@@ -20,42 +28,251 @@ public final class VxDataSerializers {
     private static int nextId = 0;
 
     public static final VxDataSerializer<Byte> BYTE = register(new VxDataSerializer<>() {
-        @Override public void write(VxByteBuf buf, Byte value) { buf.writeByte(value); }
-        @Override public Byte read(VxByteBuf buf) { return buf.readByte(); }
-        @Override public Byte copy(Byte value) { return value; }
+        @Override
+        public void write(VxByteBuf buf, Byte value) {
+            buf.writeByte(value);
+        }
+
+        @Override
+        public Byte read(VxByteBuf buf) {
+            return buf.readByte();
+        }
+
+        @Override
+        public Byte copy(Byte value) {
+            return value;
+        }
     });
 
     public static final VxDataSerializer<Integer> INTEGER = register(new VxDataSerializer<>() {
-        @Override public void write(VxByteBuf buf, Integer value) { buf.writeVarInt(value); }
-        @Override public Integer read(VxByteBuf buf) { return buf.readVarInt(); }
-        @Override public Integer copy(Integer value) { return value; }
+        @Override
+        public void write(VxByteBuf buf, Integer value) {
+            buf.writeVarInt(value);
+        }
+
+        @Override
+        public Integer read(VxByteBuf buf) {
+            return buf.readVarInt();
+        }
+
+        @Override
+        public Integer copy(Integer value) {
+            return value;
+        }
     });
 
     public static final VxDataSerializer<Float> FLOAT = register(new VxDataSerializer<>() {
-        @Override public void write(VxByteBuf buf, Float value) { buf.writeFloat(value); }
-        @Override public Float read(VxByteBuf buf) { return buf.readFloat(); }
-        @Override public Float copy(Float value) { return value; }
+        @Override
+        public void write(VxByteBuf buf, Float value) {
+            buf.writeFloat(value);
+        }
+
+        @Override
+        public Float read(VxByteBuf buf) {
+            return buf.readFloat();
+        }
+
+        @Override
+        public Float copy(Float value) {
+            return value;
+        }
     });
 
     public static final VxDataSerializer<Boolean> BOOLEAN = register(new VxDataSerializer<>() {
-        @Override public void write(VxByteBuf buf, Boolean value) { buf.writeBoolean(value); }
-        @Override public Boolean read(VxByteBuf buf) { return buf.readBoolean(); }
-        @Override public Boolean copy(Boolean value) { return value; }
+        @Override
+        public void write(VxByteBuf buf, Boolean value) {
+            buf.writeBoolean(value);
+        }
+
+        @Override
+        public Boolean read(VxByteBuf buf) {
+            return buf.readBoolean();
+        }
+
+        @Override
+        public Boolean copy(Boolean value) {
+            return value;
+        }
     });
 
     public static final VxDataSerializer<RVec3> RVEC3 = register(new VxDataSerializer<>() {
-        @Override public void write(VxByteBuf buf, RVec3 value) { buf.writeRVec3(value); }
-        @Override public RVec3 read(VxByteBuf buf) { return buf.readRVec3(); }
-        @Override public RVec3 copy(RVec3 value) { return new RVec3(value); }
+        @Override
+        public void write(VxByteBuf buf, RVec3 value) {
+            buf.writeRVec3(value);
+        }
+
+        @Override
+        public RVec3 read(VxByteBuf buf) {
+            return buf.readRVec3();
+        }
+
+        @Override
+        public RVec3 copy(RVec3 value) {
+            return new RVec3(value);
+        }
     });
 
     public static final VxDataSerializer<Quat> QUAT = register(new VxDataSerializer<>() {
-        @Override public void write(VxByteBuf buf, Quat value) { buf.writeQuat(value); }
-        @Override public Quat read(VxByteBuf buf) { return buf.readQuat(); }
-        @Override public Quat copy(Quat value) { return new Quat(value); }
+        @Override
+        public void write(VxByteBuf buf, Quat value) {
+            buf.writeQuat(value);
+        }
+
+        @Override
+        public Quat read(VxByteBuf buf) {
+            return buf.readQuat();
+        }
+
+        @Override
+        public Quat copy(Quat value) {
+            return new Quat(value);
+        }
     });
 
-    private VxDataSerializers() {}
+    public static final VxDataSerializer<Vec3> VEC3 = register(new VxDataSerializer<>() {
+        @Override
+        public void write(VxByteBuf buf, Vec3 value) {
+            buf.writeVec3(value);
+        }
+
+        @Override
+        public Vec3 read(VxByteBuf buf) {
+            return buf.readVec3();
+        }
+
+        @Override
+        public Vec3 copy(Vec3 value) {
+            return new Vec3(value);
+        }
+    });
+
+    public static final VxDataSerializer<Vec4> VEC4 = register(new VxDataSerializer<>() {
+        @Override
+        public void write(VxByteBuf buf, Vec4 value) {
+            buf.writeVec4(value);
+        }
+
+        @Override
+        public Vec4 read(VxByteBuf buf) {
+            return buf.readVec4();
+        }
+
+        @Override
+        public Vec4 copy(Vec4 value) {
+            return new Vec4(value);
+        }
+    });
+
+    public static final VxDataSerializer<Float2> FLOAT2 = register(new VxDataSerializer<>() {
+        @Override
+        public void write(VxByteBuf buf, Float2 value) {
+            buf.writeFloat2(value);
+        }
+
+        @Override
+        public Float2 read(VxByteBuf buf) {
+            return buf.readFloat2();
+        }
+
+        @Override
+        public Float2 copy(Float2 value) {
+            return new Float2(value);
+        }
+    });
+
+    public static final VxDataSerializer<Float3> FLOAT3 = register(new VxDataSerializer<>() {
+        @Override
+        public void write(VxByteBuf buf, Float3 value) {
+            buf.writeFloat3(value);
+        }
+
+        @Override
+        public Float3 read(VxByteBuf buf) {
+            return buf.readFloat3();
+        }
+
+        @Override
+        public Float3 copy(Float3 value) {
+            return new Float3(value);
+        }
+    });
+
+    public static final VxDataSerializer<UVec4> UVEC4 = register(new VxDataSerializer<>() {
+        @Override
+        public void write(VxByteBuf buf, UVec4 value) {
+            buf.writeUVec4(value);
+        }
+
+        @Override
+        public UVec4 read(VxByteBuf buf) {
+            return buf.readUVec4();
+        }
+
+        @Override
+        public UVec4 copy(UVec4 value) {
+            return new UVec4(value);
+        }
+    });
+
+    public static final VxDataSerializer<Plane> PLANE = register(new VxDataSerializer<>() {
+        @Override
+        public void write(VxByteBuf buf, Plane value) {
+            buf.writePlane(value);
+        }
+
+        @Override
+        public Plane read(VxByteBuf buf) {
+            return buf.readPlane();
+        }
+
+        @Override
+        public Plane copy(Plane value) {
+            return new Plane(value);
+        }
+    });
+
+    public static final VxDataSerializer<Color> COLOR = register(new VxDataSerializer<>() {
+        @Override
+        public void write(VxByteBuf buf, Color value) {
+            buf.writeColor(value);
+        }
+
+        @Override
+        public Color read(VxByteBuf buf) {
+            return buf.readColor();
+        }
+
+        @Override
+        public Color copy(Color value) {
+            return new Color(value);
+        }
+    });
+
+    public static final VxDataSerializer<VertexList> VERTEX_LIST = register(new VxDataSerializer<>() {
+        @Override
+        public void write(VxByteBuf buf, VertexList value) {
+            buf.writeVertexList(value);
+        }
+
+        @Override
+        public VertexList read(VxByteBuf buf) {
+            return buf.readVertexList();
+        }
+
+        @Override
+        public VertexList copy(VertexList value) {
+            VertexList copy = new VertexList();
+            int size = value.size();
+            copy.resize(size);
+            for (int i = 0; i < size; i++) {
+                copy.set(i, new Float3(value.get(i)));
+            }
+            return copy;
+        }
+    });
+
+    private VxDataSerializers() {
+    }
 
     private static <T> VxDataSerializer<T> register(VxDataSerializer<T> serializer) {
         REGISTRY.put(nextId++, serializer);
