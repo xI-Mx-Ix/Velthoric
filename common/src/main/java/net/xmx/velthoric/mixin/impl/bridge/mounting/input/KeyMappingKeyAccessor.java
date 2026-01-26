6dc702debcc968de1c444e2bbfe0789a74d8a2e3
timtaran <tentaranka@gmail.com>
2026-01-26 18:02:37 +0500

    feat: add new `VxKeyMapping` to display fallback key name if not bind, add license attribution at header, add javadoc

--- common/src/main/java/net/xmx/velthoric/mixin/impl/bridge/mounting/input/KeyMappingKeyAccessor.java
+++ common/src/main/java/net/xmx/velthoric/mixin/impl/bridge/mounting/input/KeyMappingKeyAccessor.java
@@ -1,3 +1,7 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
 package net.xmx.velthoric.mixin.impl.bridge.mounting.input;
 
 import com.mojang.blaze3d.platform.InputConstants;
@@ -5,6 +9,11 @@ import net.minecraft.client.KeyMapping;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+/**
+ * Mixin accessor to access key value of KeyMapping.
+ *
+ * @author timtaran
+ */
 @Mixin(KeyMapping.class)
 public interface KeyMappingKeyAccessor {
     @Accessor("key")
